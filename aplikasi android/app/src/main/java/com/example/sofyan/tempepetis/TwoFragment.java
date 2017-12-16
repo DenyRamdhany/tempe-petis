1542435e27d630e915165aee50ca6711dcad93c8
DenyRamdhany <deny_ramdhany@apps.ipb.ac.id>
2017-12-17 01:58:59 +0700

    Udah bisa login dari hape

--- aplikasi android/app/src/main/java/com/example/sofyan/tempepetis/TwoFragment.java
+++ aplikasi android/app/src/main/java/com/example/sofyan/tempepetis/TwoFragment.java
@@ -1,17 +1,47 @@
 package com.example.sofyan.tempepetis;
 
+import android.app.Activity;
+import android.app.Fragment;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.view.LayoutInflater;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
+
 /**
  * Created by Sofyan on 27/11/2017.
  */
 
 public class TwoFragment extends android.support.v4.app.Fragment {
+    UserPref up   = new UserPref();
+    Pelanggan pel = new Pelanggan();
+
+    private TextView txtDaya;
+
     public TwoFragment(){}
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup content, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_two, content, false);
+        View v = inflater.inflate(R.layout.fragment_two, content,false);
+        txtDaya = (TextView) v.findViewById(R.id.daya);
+
+        pel.updateData(getContext(),up.getstring(getContext(),"address"));
+        JSONObject met = null;
+
+        try {
+            met = new JSONObject(up.getstring(getContext(),"meteran"));
+
+            txtDaya.setText(met.getString("sisa_kwh").toString());
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return v;
     }
 }
